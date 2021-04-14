b2e3049bb7ac633a44505ae5367c5edb3a69405c
Ravi <raviteja.valluri@rackspace.com>
2021-04-14 11:44:39 -0700

    HTML for Search, Header and Footer

--- docs/conf.py
+++ docs/conf.py
@@ -180,10 +180,10 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "Professional Services Service Block Descriptions"
+# html_title = "Professional Services Service Block Descriptions"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = "Service Block Descriptions"
+# html_short_title = "Service Block Descriptions"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
